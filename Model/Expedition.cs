8ded7cc9c2806265a29ef7163061e11fe883303d
Mary <mariya.vorobyova94@gmail.com>
2016-04-11 02:43:41 +0300

    Ivan showed how to make binding and how Prism.Mvvm is working and how to solve the problem with update data on the form
    next step: to amend in program code with resharper
    to make real class wiew model for Expedition class

--- Model/Expedition.cs
+++ Model/Expedition.cs
@@ -4,15 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Model
 {
-    public class Expedition
+    public class Expedition : INotifyPropertyChanged
     {
+        public string name;
+
         [Key]
         public int Id { get; set; }
 
-        public string Name { get; set; }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                //эту функцию надо вызывать везде где изменяем данные свойств
+                OnPropertyChanged();
+            }
+        }
 
         [Displayable(true)]
         [Display(Name = "Цель")]
@@ -22,6 +38,18 @@ namespace Model
         [Display(Name = "Широта")]
         public double Latitude { get; set; }
 
+        
+        public double MyProperty => Latitude + 1;
+
         public virtual ICollection<Day> Days { get; set; }
+
+        //это событие оповещает о том что поле изменилось
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName]/*<- это нужно чтобы propertyName задавалось автоматически 
+        именем свойства из которого была вызвана эта функция*/ string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
